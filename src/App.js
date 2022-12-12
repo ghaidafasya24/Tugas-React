5b3c5df76dcf527a6603a2e99457245cb96068a0
ghaidafasya24 <ghaidafasya5@gmail.com>
2022-12-12 16:59:15 +0700

    Tugas React

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Ghaida Fasya Yuthika Afifah
+        </p>
+        <a
+          className="App-link"
+          href="https://instagram.com/ghaidafasya24"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sosmed
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
